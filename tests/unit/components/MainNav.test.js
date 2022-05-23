e29eb1842e0f7e025f3c4a0fa8d499678e8f2f83
KhalfaouiAnis <aniskhalfaoui9@gmail.com>
2022-05-23 23:02:59 +0100

    ActionButton component and tests

--- tests/unit/components/MainNav.test.js
+++ tests/unit/components/MainNav.test.js
@@ -6,4 +6,42 @@ describe('MainNav', () => {
     const wrapper = mount(MainNav)
     expect(wrapper.text()).toMatch('Careers')
   })
+
+  it('displays menu items for navigation', () => {
+    const wrapper = mount(MainNav)
+    const navigationMenuItems = wrapper.findAll(
+      '[data-test="main-nav-list-item"]'
+    )
+    const navigationMenuTexts = navigationMenuItems.map((item) => item.text())
+    expect(navigationMenuTexts).toEqual([
+      'Teams',
+      'Locations',
+      'Life at Careers',
+      'How we hire',
+      'Students',
+      'Jobs',
+    ])
+  })
+
+  describe('when user is logged out', () => {
+    it('prompts user to sign in', () => {
+      const wrapper = mount(MainNav)
+      const loginButton = wrapper.find('[data-test="login-button"]')
+      expect(loginButton.exists()).toBe(true)
+    })
+  })
+
+  describe('when user logs in', () => {
+    it('displays user profile picture', async () => {
+      const wrapper = mount(MainNav)
+      let profileImage = wrapper.find('[data-test="profile-image"]')
+      expect(profileImage.exists()).toBe(false)
+
+      const loginButton = wrapper.find('[data-test="login-button"]')
+      await loginButton.trigger('click')
+      profileImage = wrapper.find('[data-test="profile-image"]')
+
+      expect(profileImage.exists()).toBe(true)
+    })
+  })
 })
